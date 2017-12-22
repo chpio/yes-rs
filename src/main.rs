b8e904fbf55eeca8145bc0f9b301fe51be36b7a4
Thomas Heck <t@b128.net>
2017-12-22 23:49:46 +0100

    calc buf len

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::io::{self, Write};
 use std::env::args;
 use std::iter::once;
 
+const BUF_LEN: usize = 8 * 1024;
+
 fn main() {
     let s: String = if let Some(arg) = args().skip(1).next() {
         once(arg.as_str())
             .chain(once("\n"))
             .cycle()
-            .take(8192)
+            .take(BUF_LEN / (arg.len() + 1))
             .collect()
     } else {
-        once("y\n").cycle().take(8192).collect()
+        once("y\n").cycle().take(BUF_LEN / 2).collect()
     };
 
     let stdout = io::stdout();
